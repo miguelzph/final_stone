f990a3d899d23c5dca12a102baec51eff8aef4f1
miguelzph <miguelzph@gmail.com>
2022-08-25 14:11:58 -0300

    separation of function from database connection

---
+++ database/query_functions.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+def query_data(query):
+    """Executa uma query no banco de dados e retorna o resultado em formato de dataframe. 
+    A conexão é criada e fechada dentro da função.
+
+    Args:
+        query (text): query a ser executada no banco de dados
+
+    Returns:
+        pd.DataFrame: Retorna um dataframe com o resultado na query
+    """
+        
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+    database = os.getenv("DATABASE")
+    username = os.getenv("DB_USERNAME")
+    password = os.getenv("PASSWORD")
+
+    conn = psycopg2.connect(host=host, 
+                port=port, 
+                user=username, 
+                password=password, 
+                database=database, 
+                options="-c search_path=collect_lite,public")
+
+    df = pd.read_sql_query(query, conn)
+    
+    conn.close()
+    
+    return df
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
-import os
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import psycopg2
-from dotenv import load_dotenv
-from query import QUERY_PRODUCTS, QUERY_SALES
-from tabs.products import show_products_page
 
-load_dotenv() 
+from database.query_texts import QUERY_PRODUCTS, QUERY_SALES
+from database.query_functions import query_data
+
+from tabs.products import show_products_page
+from tabs.clients import show_clients_page
+
 
 st.set_page_config(layout='wide')
 
@@ -15,28 +15,6 @@ st.title('Statistics Lite')
 
 welcolme_page, clients_page, products_page = st.tabs(['Bem Vindo!', 'Clientes', 'Produtos'])
 
-def query_data(query):
-        
-        host = os.getenv("HOST")
-        port = os.getenv("PORT")
-        database = os.getenv("DATABASE")
-        username = os.getenv("DB_USERNAME")
-        password = os.getenv("PASSWORD")
-
-        conn = psycopg2.connect(host=host, 
-                 port=port, 
-                 user=username, 
-                 password=password, 
-                 database=database, 
-                 options="-c search_path=collect_lite,public")
-    
-        df = pd.read_sql_query(query, conn)
-        
-        conn.close()
-        
-        return df
-    
-    
 if __name__ == '__main__':
     
     with welcolme_page:
@@ -44,5 +22,7 @@ if __name__ == '__main__':
         
     data_for_products_page = query_data(QUERY_PRODUCTS)
     show_products_page(products_page, data_for_products_page)
+    
+    show_clients_page(clients_page, data_for_products_page)
     
     
